1014f713d8f42bf8ea8c8315af7cddf81a8c30db
Katie-N <kaitlyn.nordberg@gmail.com>
2024-04-13 14:07:44 -0500

    Button positioned over flier on bulletin board

--- src/components/BulletinBoard.vue
+++ src/components/BulletinBoard.vue
@@ -2,6 +2,7 @@
   <div class="absolute left-0 right-0 top-0 bottom-0 w-52 h-52 pb-12 m-auto">
     <img src="../assets/images/Bulletin Board.png">
     <button class="absolute right-6 top-8 w-24 h-16 bg-white"></button>
+    <button class="absolute left-6 top-12 w-10 h-12 bg-black"></button>
   </div>
 </template>
 <script>
