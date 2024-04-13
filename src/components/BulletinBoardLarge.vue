0945d7ef730879f9ebeeee2439fe3006b018deac
Katie-N <kaitlyn.nordberg@gmail.com>
2024-04-13 16:04:40 -0500

    Finished Bulletin Board Textbox

--- src/components/BulletinBoardLarge.vue
+++ src/components/BulletinBoardLarge.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="absolute left-0 right-0 top-0 bottom-0 w-full h-full p-12 m-auto">
     <img class="w-full h-full" src="../assets/images/Bulletin Board.png">
-    <button class="absolute right-44 top-52 w-5/12 h-60 bg-yellow-900"></button>
-    <button class="absolute left-44 top-60 w-52 h-52 bg-yellow-200"></button>
-    <button class="absolute left-64 top-36 w-20 h-16 bg-yellow-400"></button>
+    <button @click="displayWorldMap" class="absolute right-44 top-52 w-5/12 h-60 bg-transparent"></button>
+    <button @click="displayQuest" class="absolute left-44 top-60 w-52 h-52 bg-transparent"></button>
+    <button @click="displayStickyNote" class="absolute left-64 top-36 w-20 h-16 bg-transparent"></button>
+    <TextBox v-if="showChat" :text="this.text" @closeChat="showChat=false" character="crow" />
   </div>
 </template>
+
 <script>
+import TextBox from './Chat.vue';
+
 export default {
-data() {
+  components: {
+    TextBox,
+  },
+  data() {
     return {
+      text: 'Here is some placeholder text',
+      showChat: false,
+    }
+  },
+  methods: {
+    displayWorldMap() {
+      this.text = "That's a map";
+      console.log("crack")
+      this.showChat = true;
+    },
+    displayQuest() {
+      this.text = "That's a quest";
+      this.showChat = true;
+    },
+    displayStickyNote() {
+      this.text = "That's a sticky note";
+      this.showChat = true;
     }
   },
 }
