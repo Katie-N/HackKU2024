1a346e1d076e1afc2eaf0b8cafacaeccd06fb1b1
Katie-N <kaitlyn.nordberg@gmail.com>
2024-04-13 14:31:44 -0500

    All buttons added to large bulletin board

---
+++ src/components/BulletinBoardLarge.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="absolute left-0 right-0 top-0 bottom-0 w-full h-full p-12 m-auto">
+    <img class="w-full h-full" src="../assets/images/Bulletin Board.png">
+    <button class="absolute right-44 top-52 w-5/12 h-60 bg-yellow-900"></button>
+    <button class="absolute left-44 top-60 w-52 h-52 bg-yellow-200"></button>
+    <button class="absolute left-64 top-36 w-20 h-16 bg-yellow-400"></button>
+  </div>
+</template>
+<script>
+export default {
+data() {
+    return {
+    }
+  },
+}
+</script>
