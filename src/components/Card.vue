21128bb5f0add79ff1f432b8619fb920866032ea
Katie-N <kaitlyn.nordberg@gmail.com>
2024-04-13 23:21:47 -0500

    Got information on observation

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div tabindex="1" @keyup.esc="$emit('deselectObs')">
+      <div class="w-1/3 h-1/2 overflow-hidden">
+        <img :src="this.originalPhotoUrl" class="max-w-full object-cover">
+      </div>
+      <a :href="observation.taxon.wikipedia_url">{{observation.taxon.wikipedia_url}}</a>
+      <p>Taxon Name: {{observation.taxon.iconic_taxon_name}}</p>
+      <p>Common Name: {{observation.taxon.preferred_common_name}}</p>
+      <p>Observed On: {{observation.observed_on_string}}</p>
+      <p v-if="observation.place_guess">Location: {{observation.place_guess}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    originalPhotoUrl() {
+      return this.observation.photos[0].url.replace("square", "original")
+    },
+  },
+  props: ['observation'],
+  methods:{
+  },
+}
+</script>
--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -4,14 +4,14 @@
   <img src="../assets/images/CrowCaw.png" class="absolute left-37/100 top-80 w-20 z-10" @click="interactWithCrow" >
   <BulletinBoard @click="showBulletin = true; showSyncPrompt = false" class="cursor-pointer" />
   <BulletinBoardLarge v-if="showBulletin" @closeBulletinBoard="showBulletin = false" class="z-20"/>
-  <Plot class="w-56 h-24 bg-transparent absolute left-1/5 top-80" :observations="this.observations.slice(0,2).concat('').concat(this.observations.slice(2,4)).concat('').concat(this.observations.slice(4,6))" />
-  <Plot class="w-56 h-24 bg-transparent absolute right-1/5 top-80" :observations="this.observations.slice(9,12).concat('').concat(this.observations.slice(12))" />
+  <Plot class="w-56 h-24 bg-transparent absolute left-1/5 top-80" :observations="this.observations.slice(0,2).concat('').concat(this.observations.slice(2,4)).concat('').concat(this.observations.slice(4,6))" @obsSelected="this.selectedObs = $event" />
+  <Plot class="w-56 h-24 bg-transparent absolute right-1/5 top-80" :observations="this.observations.slice(9,12).concat('').concat(this.observations.slice(12))" @obsSelected="this.selectedObs = $event" />
 
   <Chat v-if="showSyncPrompt" @closeChat="showSyncPrompt=false" text="Do you have more discoveries for the island?" character="crow" interactionPrompt="Report Findings!" @promptButtonPressed="syncINat" />
 
   <Alien class="absolute w-24 left-0 right-0 m-auto top-1/2 h-min"/>
 
-  <p v-for="obs in observations" class="font-pixel bg-yellow-200">{{obs.species_guess}}</p>
+  <Card v-if="this.selectedObs" :observation="this.selectedObs" class="absolute bg-white w-screen h-screen left-0 right-0 z-30" @deselectObs="this.selectedObs=null" />
 </template>
 
 <script>
@@ -20,6 +20,8 @@ import BulletinBoardLarge from './BulletinBoardLarge.vue';
 import Plot from './Plot.vue';
 import Chat from './Chat.vue';
 import Alien from './Alien.vue';
+import Card from './Card.vue';
+
 export default {
   components: {
     BulletinBoard,
@@ -27,12 +29,14 @@ export default {
     Plot,
     Chat,
     Alien,
+    Card,
   },
   data() {
     return {
       observations: [],
       showBulletin: false,
       showSyncPrompt: false,
+      selectedObs: null,
     }
   },
   methods: {
